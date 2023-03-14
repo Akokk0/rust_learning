7ff79044e960df66f20335314f38dede805d1c4e
akokko <admin@akokko.moe>
2023-03-14 13:39:44 +0800

    life cycle learning ++

---
+++ generics_trait_lifecycle/src/23_main_lifecycle_omission.rs
@@ -0,0 +1,25 @@
+// 早期Rust必须这样写
+fn _first_word<'a>(s: &'a str) -> &'a str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
+}
+
+// 这里并没有标注生命周期
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
+}
+
+fn main() {}
