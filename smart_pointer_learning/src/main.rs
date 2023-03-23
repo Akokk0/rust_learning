8fdc8237e2838d5867bb58c5a6ac86339e1f6b4e
akokko <admin@akokko.moe>
2023-03-23 23:15:43 +0800

    smart pointer learning finish

--- smart_pointer_learning/src/main.rs
+++ smart_pointer_learning/src/main.rs
@@ -1,28 +1,53 @@
 use std::cell::RefCell;
-use std::rc::Rc;
-use crate::List::{Cons, Nil};
+use std::rc::{Rc, Weak};
 
 #[derive(Debug)]
-enum List {
-    Cons(Rc<RefCell<i32>>, Rc<List>),
-    Nil
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>
 }
 
 fn main() {
-    let value = Rc::new(RefCell::new(5));
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![])
+    });
 
-    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
-    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
-    let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
+    // println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
 
-    *value.borrow_mut() += 10;
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)])
+        });
 
-    println!("a after = {:?}", a);
-    println!("b after = {:?}", b);
-    println!("c after = {:?}", c);
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        // println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch)
+        );
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf)
+        );
+    }
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
 }
-
-/*  其他可实现内部可变性的类型
-    - Cell<T>：通过复制来访问数据
-    - Mutex<T>：用于实现跨线程情形下的内部可变性模式
-*/
