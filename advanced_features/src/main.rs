5b4387415ebfa640aa749d4dd3c2ff403fffb314
akokko <admin@akokko.moe>
2023-03-30 13:21:26 +0800

    advanced feature learning

--- advanced_features/src/main.rs
+++ advanced_features/src/main.rs
@@ -1,23 +1,35 @@
-trait Animal {
-    fn baby_name() -> String;
+/*type Kilometers = i32;
+
+fn main() {
+    let x: i32 = 5;
+    let y: Kilometers = 5;
+    println!("x + y = {}", x + y);
+}*/
+
+/*fn takes_long_type(f: Box<dyn Fn() + Send + 'static>) {
+    // --snip--
 }
 
-struct Dog;
-
-impl Dog {
-    fn baby_name() -> String {
-        String::from("Spot")
-    }
-}
-
-impl Animal for Dog {
-    fn baby_name() -> String {
-        String::from("puppy")
-    }
+fn returns_long_type() -> Box<dyn Fn() + Send + 'static> {
+    Box::new(|| println!("hi"))
 }
 
 fn main() {
-    println!("A baby dog is called a {}", Dog::baby_name());
-    println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
+    let f: Box<dyn Fn() + Send + 'static> = Box::new(|| println!("hi"));
+}*/
+
+// 优化
+
+type Thunk = Box<dyn Fn() + Send + 'static>;
+
+fn takes_long_type(f: Thunk) {
+    // --snip--
 }
 
+fn returns_long_type() -> Thunk {
+    Box::new(|| println!("hi"))
+}
+
+fn main() {
+    let f: Thunk = Box::new(|| println!("hi"));
+}
