0d31df71785824b52ffdfe4a96671c66d468ed84
akokko <admin@akokko.moe>
2023-04-23 12:13:32 +0800

    bug fixed

---
+++ ws/target/debug/deps/server1-629d8427bcaefd16.d
@@ -0,0 +1,5 @@
+/Users/akokko/RustProject/ws/target/debug/deps/server1-629d8427bcaefd16.rmeta: webservice/src/bin/server1.rs
+
+/Users/akokko/RustProject/ws/target/debug/deps/server1-629d8427bcaefd16.d: webservice/src/bin/server1.rs
+
+webservice/src/bin/server1.rs:
---
+++ ws/target/debug/deps/webservice-502b915f1fafb0d7.d
@@ -0,0 +1,5 @@
+/Users/akokko/RustProject/ws/target/debug/deps/webservice-502b915f1fafb0d7.rmeta: webservice/src/main.rs
+
+/Users/akokko/RustProject/ws/target/debug/deps/webservice-502b915f1fafb0d7.d: webservice/src/main.rs
+
+webservice/src/main.rs:
